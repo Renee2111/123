14ab9ff83ccfe399701191f1d5a2f0cf4b87781b
Renee2111 <123558647+Renee2111@users.noreply.github.com>
2026-03-01 20:49:36 +0700

    quoc pro vip

---
+++ nhap.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //freopen("nhap.inp","r",stdin);
+    //freopen("nhap.out","w",stdout);
+    int a,b,n,tong;
+    cin>>a>>b;
+    //a=1
+    tmp=a;//1
+    a=b;//2
+    b=tmp;
+    //b=2
+
+
+    cout<<a<<endl<<b;
+}
+//23-> 32  30+2= 32
+//1
+//  tmp=1%10=1            m=n%10
+//  1/10=0                  n=n/10
+//  3*10+2=32*10+1=321    tong=tong *10+m;
+//tong: 321
